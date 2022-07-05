7c7d509169729f5509ade1adf74164fcd2ec8489
Svetlana <burtovaya85@gmail.com>
2022-07-05 19:41:00 +0600

    Module_47 Added service

---
+++ app/src/main/java/kz/tokarev/myapplication/receivers/ConnectionChecker.kt
@@ -0,0 +1,33 @@
+package kz.tokarev.myapplication.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+
+class ConnectionChecker : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        //Если интента нет то выходим из метода
+        if (intent == null) return
+        //Проверяем какой пришел action
+        when (intent.action) {
+            //Если пришел низкий заряд батареи
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Батарея разряжена", Toast.LENGTH_SHORT).show()
+                //Включается ночная тема
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            //Если пришло подключение к зарядке
+            Intent.ACTION_POWER_CONNECTED -> {Toast.makeText(
+                context,
+                "Зарядка подключена",
+                Toast.LENGTH_SHORT
+            ).show()
+                //Режим ночной темы выключается
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        }
+    }
+}
--- app/src/main/java/kz/tokarev/myapplication/view/MainActivity.kt
+++ app/src/main/java/kz/tokarev/myapplication/view/MainActivity.kt
@@ -1,4 +1,7 @@
 package kz.tokarev.myapplication.view
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +10,12 @@ import kz.tokarev.myapplication.API
 import kz.tokarev.myapplication.R
 import kz.tokarev.myapplication.databinding.ActivityMainBinding
 import kz.tokarev.myapplication.data.Entity.Film
+import kz.tokarev.myapplication.receivers.ConnectionChecker
 import kz.tokarev.myapplication.view.fragments.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,17 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
 
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
     fun launchDetailsFragment(film: Film) {
@@ -97,4 +113,5 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
     }
+
 }
